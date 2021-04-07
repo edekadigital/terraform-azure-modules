c6f5c86a565cfee8d5523fa4e8c705ead1b4fae7
Felix Bechstein <fb@betaset.de>
2021-04-07 16:29:34 +0200

    Stop pinning specific versions
    This prevents me from upgrading tf on my main project.

--- terraform-azure-datadog-log-forwarder/versions.tf
+++ terraform-azure-datadog-log-forwarder/versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = "0.14.7"
+  required_version = ">= 0.14.0"
 
   required_providers {
-    azurerm = "2.48.0"
+    azurerm = ">= 2.48.0"
     datadog = {
       source = "DataDog/datadog"
     }
