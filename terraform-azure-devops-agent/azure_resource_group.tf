32ab20d0488a71059b386bee91cf93ec3185731c
Nico Engelen <engelen.nico@googlemail.com>
2021-11-24 11:38:01 +0100

    proper renaming of variable

--- terraform-azure-devops-agent/azure_resource_group.tf
+++ terraform-azure-devops-agent/azure_resource_group.tf
@@ -2,6 +2,6 @@ resource "azurerm_resource_group" "devops_agent" {
   count = var.azure_existing_resource_group == "" && var.azure_instance_count > 0 ? 1 : 0
 
   name     = "rg-${var.project_name_as_resource_prefix}-devops-agent"
-  location = var.resource_location
+  location = var.azure_resource_location
   tags     = var.azure_tags
 }
