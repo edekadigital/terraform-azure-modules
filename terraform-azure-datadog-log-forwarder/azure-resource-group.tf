34d97869d7f3dc163640a1bd522fe152098335e3
Vagharshak <vagharshak@Vagharshaks-MacBook-Pro.local>
2022-08-31 09:23:26 +0200

    no ticket - log forwarder is now migrated to Team Blues private GitHub repi

--- terraform-azure-datadog-log-forwarder/azure-resource-group.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "datadog" {
-  count = var.existing_ressource_group == "" ? 1 : 0
-
-  name     = "${var.project_name_as_resource_prefix}-datadog-rg"
-  location = var.resource_location
-  tags     = var.azure_tags
-}
